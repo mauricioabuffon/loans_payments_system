eb2f415282fc5f0e62482f88a84d7a66f788405d
Mauricio Antonio Buffon <mauricioabuffon@gmail.com>
2020-12-05 17:10:03 -0300

    Implementing basic program

--- src/services/UsaInterestService.java
+++ src/services/UsaInterestService.java
@@ -2,18 +2,18 @@ package services;
 
 import java.security.InvalidParameterException;
 
-public class BrazilInterestService implements InterestService{
+public class UsaInterestService implements InterestService{
 
 	private double interestRate;
 
-	public BrazilInterestService(double interestRate) {
+	public UsaInterestService(double interestRate) {
 		this.interestRate = interestRate;
 	}
-@Override
+	@Override
 	public double getInterestRate() {
 		return interestRate;
 	}
-@Override
+	@Override
 	public double payment(double amount, int months) {
 		if (months < 1) {
 			throw new InvalidParameterException("Months must be greater than zero");
