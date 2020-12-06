8f574be8f9c21c2defc318c07834b0c1dd12412c
Mauricio Antonio Buffon <mauricioabuffon@gmail.com>
2020-12-06 11:23:41 -0300

    Adjusting

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import services.BrazilInterestService;
 import services.InterestService;
 import services.UsaInterestService;
 
@@ -16,9 +17,13 @@ public class Program {
 		double amount = sc.nextDouble();
 		System.out.print("Months: ");
 		int months = sc.nextInt();
-
-//		BrazilInterestService is = new BrazilInterestService(2.0);
+//		System.out.print("1 - Brazil or 2 - Usa: ");
+//		int opt = sc.nextInt();
+//		if (opt == 1) {
+//			InterestService is = new BrazilInterestService(2.0);
+//		} else {
 		InterestService is = new UsaInterestService(1.0);
+//		}
 		double payment = is.payment(amount, months);
 
 		System.out.println("Payment after " + months + " months:");
@@ -26,7 +31,4 @@ public class Program {
 
 		sc.close();
 	}
-
-	}
-
-
+}
